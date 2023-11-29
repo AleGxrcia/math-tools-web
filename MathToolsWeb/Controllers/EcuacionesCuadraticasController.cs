06f8e8e5ab0bc9dd4648b2c8b05fba9bec93bffe
AleGxrcia <fagarciatavarez@gmail.com>
2023-11-28 23:27:30 -0400

    Añadiendo Controller de Ecuaciones

---
+++ MathToolsWeb/Controllers/EcuacionesCuadraticasController.cs
@@ -0,0 +1,37 @@
+﻿using MathToolsWeb.Models;
+using MathToolsWeb.Servicios;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace MathToolsWeb.Controllers
+{
+    public class EcuacionesCuadraticasController : Controller
+    {
+        private readonly IServicioEcuaciones servicioEcuaciones;
+
+        public EcuacionesCuadraticasController(IServicioEcuaciones servicioEcuaciones)
+        {
+            this.servicioEcuaciones = servicioEcuaciones;
+        }
+
+        public IActionResult Calcular()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calcular(EcuacionCuadratica ecuacion)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(ecuacion);
+            }
+
+            var pasos = servicioEcuaciones.CalcularEcuacionCuadratica(ecuacion);
+            ViewBag.Pasos = pasos;
+            ViewBag.Ecuacion = ecuacion;
+
+            return View(ecuacion);
+        }
+    }
+}
