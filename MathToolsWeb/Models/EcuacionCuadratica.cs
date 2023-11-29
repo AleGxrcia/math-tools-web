a35056b8e21f4f936f480bb878c288ea5433b152
AleGxrcia <fagarciatavarez@gmail.com>
2023-11-28 23:22:40 -0400

    Clase modelo de Ecuacion Cuadratica

---
+++ MathToolsWeb/Models/EcuacionCuadratica.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MathToolsWeb.Models
+{
+    public class EcuacionCuadratica
+    {
+        [Required(ErrorMessage = "El termino a es requerido")]
+        [Display(Name = "a:")]
+        public double A { get; set; }
+        [Required(ErrorMessage = "El termino b es requerido")]
+        [Display(Name = "b:")]
+        public double B { get; set; }
+        [Required(ErrorMessage = "El termino c es requerido")]
+        [Display(Name = "c:")]
+        public double C { get; set; }
+    }
+}
