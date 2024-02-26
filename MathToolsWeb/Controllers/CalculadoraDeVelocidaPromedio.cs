8279f62c8779344ff565757d5700b1701a8c1f77
AleGxrcia <fagarciatavarez@gmail.com>
2024-02-26 15:54:58 -0400

    Arreglos de velocidad promerio

---
+++ MathToolsWeb/Controllers/CalculadoraDeVelocidaPromedio.cs
@@ -0,0 +1,37 @@
+﻿using MathToolsWeb.Models;
+using MathToolsWeb.Servicios;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MathToolsWeb.Controllers
+{
+    public class VelocidadPromedioController : Controller
+    {
+        private readonly IServicioVelocidad servicioVelocidad;
+
+        public VelocidadPromedioController(IServicioVelocidad servicioVelocidad)
+        {
+            this.servicioVelocidad = servicioVelocidad;
+        }
+
+        public IActionResult Calcular()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calcular(DatosVelocidad datos)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(datos);
+            }
+
+            var resultado = servicioVelocidad.CalcularVelocidadPromedio(datos);
+            ViewBag.Resultado = resultado;
+            ViewBag.Datos = datos;
+            ViewBag.Unidades = Request.Form["unidadVelocidad"];
+
+            return View(datos);
+        }
+    }
+}
---
+++ MathToolsWeb/Models/CalculadoraDeVelocidaPromedio.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MathToolsWeb.Models
+{
+    public class DatosVelocidad
+    {
+        [Required(ErrorMessage = "La velocidad inicial es requerida")]
+        [Display(Name = "Velocidad Inicial:")]
+        public double VelocidadInicial { get; set; }
+
+        [Required(ErrorMessage = "La velocidad final es requerida")]
+        [Display(Name = "Velocidad Final:")]
+        public double VelocidadFinal { get; set; }
+
+        [Required(ErrorMessage = "El tiempo es requerido")]
+        [Display(Name = "Tiempo:")]
+        public double Tiempo { get; set; }
+    }
+}
