6e1af101be96c3c520a8aef85b09a64ab949197c
AleGxrcia <fagarciatavarez@gmail.com>
2024-02-26 15:57:00 -0400

    Agregando funcionalidad de graficar parabolas

---
+++ MathToolsWeb/Controllers/GraficarFuncionesController.cs
@@ -0,0 +1,48 @@
+﻿using MathToolsWeb.Models;
+using MathToolsWeb.Servicios;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MathToolsWeb.Controllers
+{
+    public class GraficarFuncionesController : Controller
+    {
+        private readonly IServicioEcuaciones servicioEcuaciones;
+
+        public GraficarFuncionesController(IServicioEcuaciones servicioEcuaciones)
+        {
+            this.servicioEcuaciones = servicioEcuaciones;
+        }
+
+        [HttpGet]
+        public IActionResult GraficarParabola()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Graficar(EcuacionCuadratica ecuacion)
+        {
+            var punto = 2;
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var interseccionX = servicioEcuaciones.InterseccionX(ecuacion);
+            var interseccionY = servicioEcuaciones.InterseccionY(ecuacion);
+            var vertice = servicioEcuaciones.Vertice(ecuacion);
+            var rectaTan = servicioEcuaciones.RectaTangente(ecuacion, punto);
+
+            ViewBag.InterseccionX = interseccionX;
+            ViewBag.InterseccionY = interseccionY;
+            ViewBag.Vertice = vertice;
+            ViewBag.Ecuacion = ecuacion;
+            ViewBag.Pendiente = rectaTan.Item1;
+            ViewBag.PuntoPendiente = rectaTan.Item2;
+
+            return View("ResultadosParabola", ecuacion);
+        }
+
+    }
+}
